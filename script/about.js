46e90122db5a9db8b27f4fae98755be6f4a4efe8
Yasin Z <55712458+Ysnz@users.noreply.github.com>
2020-11-10 20:04:30 +0300

    Update about.js

--- script/about.js
+++ script/about.js
@@ -1,4 +1,4 @@
-var about = "I'm Deepanshu tyagi,currently persuing Btech in Computer science engineering from Dr.APJ abdul kalam technical University.I'm a Web developer and learning data science with keen understanding of Python,html,css,django,data structure.";
+var about = "'m Yasin. I'm FullStack Web developer and learning data science with keen understanding of Python,html,css,django,data structure.";
 i = 0;
 function typeWriter() {
     if (i < about.length) {
